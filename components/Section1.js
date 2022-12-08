0a5a3cfbfaf654701512717a0d16b5d143b9e1a0
Ashraful Islam <ashrafunit7@gmail.com>
2022-12-08 16:05:18 +0600

    build:section1

---
+++ components/Section1.js
@@ -0,0 +1,38 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+export default function Section1() {
+    return (
+        <section className="py-16">
+            <div className="container mx:auto md:px-20">
+                <h1 className="font-bold text-4xl pb-12 text-center">Trending</h1>
+                {
+                    Slide()
+                }
+            </div>
+        </section>
+    )
+}
+
+function Slide() {
+    return (
+        <div className='grid md:grid-cols-2'>
+            <div className='image'>
+            <Link legacyBehavior href={"/"}><a><Image src={"/images/img1.jpg"} width={600} height={600} alt="" /></a></Link>
+            </div>
+
+            <div className='info flex justify-center flex-col'>
+                <div className="category">
+                    <Link legacyBehavior href={"/"}><a className="text-orange-600 hover:text-orange-800">Business, travel </a></Link>
+                    <Link legacyBehavior href={"/"}><a className="text-gray-800 hover:text-gray-600">- December 08</a></Link>
+                </div> 
+                <div className="title">
+                    <Link legacyBehavior href={"/"}><a className="text-3xl md:text-6xl font-bold text-gray-800 hover:text-gray-600 text-justify">Your most unhappy customers are the best source of learning and improving!</a></Link>
+                </div>
+                <p className="text-gray-500 py-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga doloribus repellat eligendi, quibusdam aut inventore est at quo ad facere.</p>
+                <h1>author</h1>
+            </div>
+
+        </div>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 import Footer from '../components/Footer'
 import Header from '../components/Header'
+import Section1 from '../components/Section1'
 import Format from '../layout/format'
 
 export default function Home() {
   return (
-    <Format />
+    <Format>
+      <Section1/>
+    </Format>
   )
 }
