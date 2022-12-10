f2a43346d2ecfad76c18c7346b8556eeee3757dd
Ashraful Islam <ashrafunit7@gmail.com>
2022-12-10 17:12:55 +0600

    new file:   lib/fetcher.js 	new file:   lib/helper.js

--- components/Section2.js
+++ components/Section2.js
@@ -1,8 +1,12 @@
 import Image from "next/image"
 import Link from "next/link"
+import getPost from "../lib/helper"
 import Author from "./_child/Author"
+import fetcher from '../lib/fetcher'
 
 export default function Section2() {
+    const {data, isLoading, isError} = fetcher('api/posts')
+    if(data) console.log(data)
   return (
     <section className="container mx-auto md:px-20 py-10">
         <h1 className="font-bold text-4xl py-12 text-center">Latest Posts</h1>
