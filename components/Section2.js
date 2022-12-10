f50b4f9741d92298c39aeeaaf21a84fccace1922
Ashraful Islam <ashrafunit7@gmail.com>
2022-12-10 18:00:55 +0600

    new file:   components/_child/Error.js 	new file:   components/_child/Spinner.js

--- components/Section2.js
+++ components/Section2.js
@@ -3,47 +3,50 @@ import Link from "next/link"
 import getPost from "../lib/helper"
 import Author from "./_child/Author"
 import fetcher from '../lib/fetcher'
+import Spinner from "./_child/Spinner"
+import Error from "./_child/Error"
 
 export default function Section2() {
     const {data, isLoading, isError} = fetcher('api/posts')
-    if(data) console.log(data)
+
+    if(isLoading) return <Spinner/>
+    if(isError) return <Error/>
+    
   return (
     <section className="container mx-auto md:px-20 py-10">
         <h1 className="font-bold text-4xl py-12 text-center">Latest Posts</h1>
 
         {/* grid columns */}
         <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-14">
-            { Post() }
-            { Post() }
-            { Post() }
-            { Post() }
-            { Post() }
-            { Post() }
-            { Post() }
+            {
+                data?.map((post, index) => <Post data={post} key={index} />)
+            }
         </div>
     </section>
   )
 }
 
-function Post(){
+function Post({data}){
+    const {id, category, img, published, author, title, subtitle} = data
     return (
         <div className="item">
             <div className="images">
-                <Link legacyBehavior href={"/"}><a><Image src={"/images/img1.jpg"} className="rounded" width={500} height={350} alt=""/></a></Link>
+                <Link legacyBehavior href={"/"}><a><Image src={img || '/'} className="rounded" width={500} height={350} alt=""/></a></Link>
             </div>
             <div className="info flex justify-center flex-col py-4">
                 <div className="cat">
-                    <Link legacyBehavior href={"/"}><a className="text-orange-600 hover:text-orange-800">Business, Travel</a></Link>
-                    <Link legacyBehavior href={"/"}><a className="text-gray-800 hover:text-gray-600">- July 3, 2022</a></Link>
+                    <Link legacyBehavior href={"/"}><a className="text-orange-600 hover:text-orange-800">{category || 'unknown'}</a></Link>
+                    <Link legacyBehavior href={"/"}><a className="text-gray-800 hover:text-gray-600">- {published || 'unknown'}</a></Link>
                 </div>
                 <div className="title">
-                    <Link legacyBehavior href={"/"}><a className="text-xl font-bold text-gray-800 hover:text-gray-600">Your most unhappy customers are your greatest source of learning</a></Link>
+                    <Link legacyBehavior href={"/"}><a className="text-xl font-bold text-gray-800 hover:text-gray-600">{title || 'title'}</a></Link>
                 </div>
                 <p className="text-gray-500 py-3">
-                    Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind 
-                    text by the name of Lorem Ipsum decided to leave for the far World of Grammar.
+                    {subtitle || 'subtitle'}
                 </p>
-                <Author></Author>
+                {
+                    author && <Author></Author>
+                }
             </div>
         </div>
     )
