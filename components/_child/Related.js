c561f767ead6ae945291cdd9bc2b6424a67ad920
Ashraful Islam <ashrafunit7@gmail.com>
2022-12-11 17:45:08 +0600

    refactor: alt issue in img

--- components/_child/Related.js
+++ components/_child/Related.js
@@ -23,7 +23,7 @@ function Post(){
     return (
         <div className="flex gap-5">
             <div className="image flex flex-col justify-start">
-                <Link legacyBehavior href={"/"}><a><Image src={"/images/img1.jpg"} className="rounded" width={300} height={200} /></a></Link>
+                <Link legacyBehavior href={"/"}><a><Image src={"/images/img1.jpg"} className="rounded" width={300} height={200} alt=""/></a></Link>
             </div>
             <div className="info flex justify-center flex-col">
                 <div className="cat">
